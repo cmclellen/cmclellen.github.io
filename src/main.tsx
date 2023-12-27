d755a6c785c00d6c861103b97406cc05a7b57767
acmac <craig.mclellen@eighty20solutions.com.au>
2023-12-28 05:31:50 +0800

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
 import { BrowserRouter } from 'react-router-dom'
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
