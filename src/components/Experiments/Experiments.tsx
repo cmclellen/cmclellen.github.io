85e2e60928bcdc1b72fdd7c7e03caf72724dcd1a
acmac <craig.mclellen@eighty20solutions.com.au>
2023-12-28 09:10:13 +0800

    Fix

--- src/components/Experiments/Experiments.tsx
+++ src/components/Experiments/Experiments.tsx
@@ -7,22 +7,21 @@ interface ExperimentsProps { }
 const Experiments: FC<ExperimentsProps> = () => (
    <ExperimentsWrapper>
       <div className="container py-5">
-      <div className="row row-cols-1 row-cols-md-3 g-4">
-         <div className="col">
-            <div className="card">
-               <img src={reservationimg} />
-               <div className="card-body">
-                  <h5 className="card-title">Reservation System</h5>
-                  <p className="card-text">Decide where you want to go and leave the flight, car rental and hotel bookings to us.</p>
-               </div>
-               <div className="card-footer">
-                  <a href="/reservation/">Try it!</a>
+         <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="col">
+               <div className="card">
+                  <img src={reservationimg} />
+                  <div className="card-body">
+                     <h5 className="card-title">Reservation System</h5>
+                     <p className="card-text">Decide where you want to go and leave the flight, car rental and hotel bookings to us.</p>
+                  </div>
+                  <div className="card-footer">
+                     <a href="/reservation/">Try it!</a>
+                  </div>
                </div>
             </div>
-         </div>
-         <div className="col">
+            {/* <div className="col">
             <div className="card">
-               {/* <img src="..." className="card-img-top" alt="..."> */}
                   <div className="card-body">
                      <h5 className="card-title">Card title</h5>
                      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.
@@ -37,7 +36,6 @@ const Experiments: FC<ExperimentsProps> = () => (
          </div>
          <div className="col">
             <div className="card">
-               {/* <img src="..." className="card-img-top" alt="..."> */}
                   <div className="card-body">
                      <h5 className="card-title">Card title</h5>
                      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
@@ -47,7 +45,8 @@ const Experiments: FC<ExperimentsProps> = () => (
                   </div>
             </div>
          </div>
-      </div>
+          */}
+         </div>
       </div>
    </ExperimentsWrapper>
 );
