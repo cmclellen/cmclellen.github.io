31e246a959c0969c0dc874889d5de55adf91de13
acmac <craig.mclellen@eighty20solutions.com.au>
2023-12-28 09:06:15 +0800

    Fix

--- src/components/Experiments/Experiments.tsx
+++ src/components/Experiments/Experiments.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { ExperimentsWrapper } from './Experiments.styled';
+import reservationimg from '../../assets/reservation.jpg';
 
 interface ExperimentsProps { }
 
@@ -9,13 +10,13 @@ const Experiments: FC<ExperimentsProps> = () => (
       <div className="row row-cols-1 row-cols-md-3 g-4">
          <div className="col">
             <div className="card">
-               {/* <img src="..." className="card-img-top" alt="..."> */}
+               <img src={reservationimg} />
                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <h5 className="card-title">Reservation System</h5>
+                  <p className="card-text">Decide where you want to go and leave the flight, car rental and hotel bookings to us.</p>
                </div>
                <div className="card-footer">
-                  <small className="text-muted">Last updated 3 mins ago</small>
+                  <a href="/reservation/">Try it!</a>
                </div>
             </div>
          </div>
