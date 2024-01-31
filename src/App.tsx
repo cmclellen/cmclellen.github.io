a20ea5357de77ba544d24c838cb08ea040e2600c
acmac <craig.mclellen@eighty20solutions.com.au>
2024-01-31 18:56:12 +0800

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import './App.scss'
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
-import About from './pages/about/about'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home/home'
+import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import About from "./pages/about/about";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/home";
 
 function App() {
-
   return (
     <>
       <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
