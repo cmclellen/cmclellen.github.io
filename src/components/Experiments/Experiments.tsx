7ee933602e5f23d83cbc98aa60b125874ff716d2
acmac <craig.mclellen@eighty20solutions.com.au>
2024-02-06 16:41:41 +0800

    Fix

--- src/components/Experiments/Experiments.tsx
+++ src/components/Experiments/Experiments.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { ExperimentsWrapper } from './Experiments.styled';
 import reservationimg from '../../assets/reservation.jpg';
 import auditoriumimg from '../../assets/auditorium.jpg';
+import magnifyimg from '../../assets/magnify.jpg';
 
 interface ExperimentsProps { }
 
@@ -9,6 +10,7 @@ const Experiments: FC<ExperimentsProps> = () => (
    <ExperimentsWrapper>
       <div className="container py-5">
          <div className="row row-cols-1 row-cols-md-3 g-4">
+
             <div className="col">
                <div className="card">
                   <img src={reservationimg} />
@@ -21,11 +23,12 @@ const Experiments: FC<ExperimentsProps> = () => (
                   </div>
                </div>
             </div>
+
             <div className="col">
                <div className="card">
                   <img src={auditoriumimg} />
                   <div className="card-body">
-                     <h5 className="card-title">RAG (AI) using Famous Speeches</h5>
+                     <h5 className="card-title">AI :: RAG using Famous Speeches</h5>
                      <p className="card-text">Using RAG to search within a dataset of famous speeches.</p>
                   </div>
                   <div className="card-footer">
@@ -33,6 +36,20 @@ const Experiments: FC<ExperimentsProps> = () => (
                   </div>
                </div>
             </div>
+
+            <div className="col">
+               <div className="card">
+                  <img src={magnifyimg} />
+                  <div className="card-body">
+                     <h5 className="card-title">AI :: PII Detection</h5>
+                     <p className="card-text">Using Azure AI Search & Document Intelligence to identify documents containing PII information.</p>
+                  </div>
+                  <div className="card-footer">
+                     <a href="/PiiDocDetect/">Read more</a>
+                  </div>
+               </div>
+            </div>
+
          </div>
       </div>
    </ExperimentsWrapper>
